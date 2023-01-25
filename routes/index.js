6f5600efa2e8c3e121739406b40e60b73c73846f
Luis García <pruebasdaw2021@gmail.com>
2023-01-25 13:09:56 +0100

    Eliminado index.html y listo para produccion

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,21 @@ const router = express.Router();
 const pool = require('../db.js')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  const [rows] = await pool.query('SELECT * FROM users')
+  res.json(rows)
 });
 
 router.get('/ping', async function(req, res, next) {
   const [result] = await pool.query('SELECT "hello world" as RESULT')
   //console.log(result[0])
   res.json(result[0]);
+});
+
+router.get('/create', async function(req, res, next) {
+  const result = await pool.query('INSERT INTO users(name) VALUES ("Salem")')
+  //console.log(result[0])
+  res.json(result);
 });
 
 module.exports = router;
