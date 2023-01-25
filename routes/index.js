fa9eb54134e571978fdfba557c1589d4da1862b9
Luis García <pruebasdaw2021@gmail.com>
2023-01-25 12:56:10 +0100

    Creada base de datos y tabla users

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const pool = require('../db.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/ping', async function(req, res, next) {
+  const [result] = await pool.query('SELECT "hello world" as RESULT')
+  //console.log(result[0])
+  res.json(result[0]);
 });
 
 module.exports = router;
